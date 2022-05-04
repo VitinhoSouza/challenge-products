73f8e56eff382ede74e496a64aaac32246d8d89e
VitinhoSouza <vitinhosouzajuatama@gmail.com>
2022-05-04 18:51:47 -0300

    config server.js

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-console.log("Batata");
+/* eslint-disable import/newline-after-import */
+/* eslint-disable consistent-return */
+const express = require('express');
+const {resolve} = require('path')
+const app = express();
+
+app.use('/',express.static(
+    resolve(
+        __dirname,
+    './build'
+    )
+))
+
+app.listen(process.env.PORT || 3000, (err) => {
+    if(err){
+        return console.log(err);
+    } 
+
+    console.log('Is Running!')
+}) 
