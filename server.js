e8c5ee0341e04388a436a7f371b835e711aa7041
VitinhoSouza <vitinhosouzajuatama@gmail.com>
2022-05-06 00:47:20 -0300

    Add rule in server.js file

--- server.js
+++ server.js
@@ -11,6 +11,13 @@ app.use('/',express.static(
     )
 ))
 
+if (process.env.NODE_ENV === "production") { 
+    app.use(express.static("client/build")); 
+    app.get("*", (req, res) => { 
+        req.sendFile(path.resolve(__dirname, "client", "build", "index.html")); 
+    }); 
+} 
+
 app.listen(process.env.PORT || 3000, (err) => {
     if(err){
         return console.log(err);
